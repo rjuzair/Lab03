edb0adc12f51505e42cce03f9085b53f8e7b060e
Raja Uzair Saeed <rjuzair@gmail.com>
2021-09-13 13:08:46 +0200

    first comit

---
+++ LabPackage/R/dijkstra.R
@@ -0,0 +1,62 @@
+#' @title dijkstra
+#'
+#' @param graph a graph as data frame.
+#' @param init_node the initial node.
+#' @description The algorithm calculates the shortest path of a graph \code{wiki_graph} with an initial node.
+#' @source See more at: \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#' @return The shortest path of \code{graph} with the initial node \code{init_node}
+#' @export
+#' @examples
+#' dijkstra(wiki_graph, 1)
+#' dijkstra(wiki_graph, 3)
+
+
+dijkstra <- function(graph, init_node) {
+
+  stopifnot( is.data.frame(graph) == TRUE && all(c("v1", "v2", "w") %in% colnames(graph)) == TRUE &&
+               length(init_node) == 1 && is.numeric(init_node) == TRUE &&
+               (init_node %in% graph$v1) == TRUE )
+
+  # initial values
+  Q <- 1:max(graph[, 1])
+
+  distance <- c()
+
+  for (i in 1:max(graph[, 1])) {
+    distance[i] <- Inf
+  }
+
+  distance[init_node] <- 0
+
+  node_u <- init_node
+  u <- which(graph$v1==node_u)
+
+  graph$cost <- Inf
+  graph$cost <- replace(graph$cost, u, graph$w[u])
+
+  while (length(Q) > 1) {
+
+    # calculate distance/cost
+    distance[graph$v2[u]] <- pmin(graph$cost[u], distance[graph$v2[u]])
+
+    # remove node from set Q
+    Q <- Q[!Q %in% node_u]
+
+    # update node as min value of cost column
+    node_u <- graph$v2[(which.min(graph$cost))]
+    u <- which(graph$v1==node_u)
+
+    # update distance/cost
+    graph$cost <- replace(graph$cost, u, graph$w[u]+min(distance[Q]))
+
+    # remove used row & update row and node names
+    graph <- graph[-which.min(graph$cost),]
+    rownames(graph) = seq(length=nrow(graph))
+    u <- which(graph$v1==node_u)
+
+  }
+
+  return(distance)
+
+}
+
---
+++ LabPackage/R/euclidean.R
@@ -0,0 +1,33 @@
+#' @title euclidean
+#' euclidiean calculates the Greatest Common Divisor (GCD) for 2 numbers
+#'
+#' @param b a number
+#' @param a a number
+#' @description euclidean use the euclidean algorithm to calculcate the GCD.
+#' @source
+#' see more at: \url{https://en.wikipedia.org/wiki/Euclidean_algorithm}
+#'
+#' @return The GCD of \code{b} and \code{a}.
+#' @export
+#' @examples
+#' euclidean(123612, 13892347912)
+#' euclidean(100, 1000)
+
+
+
+euclidean <-
+function(b, a) {
+
+  stopifnot( class(a) == 'numeric' && length(a) == 1 && class(b) == 'numeric' && length(b) == 1 )
+
+  while (b != 0) {
+
+    q <- b
+    b <- a %% b
+    a <- q
+
+    }
+
+  return(q)
+
+}
